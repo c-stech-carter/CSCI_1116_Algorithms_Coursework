cd5ea6bdb2550be8c99e7519ab41b1bf11a3ef0e
c-stech-carter <punkynyrd@gmail.com>
2024-12-18 18:05:48 -0700

    Update GeometricObject.java with improved Indentation

--- Chapter_23/Exercise23_03/GeometricObject.java
+++ Chapter_23/Exercise23_03/GeometricObject.java
@@ -1,42 +1,58 @@
 // GeometricObject.java: The abstract GeometricObject class
 public abstract class GeometricObject {
-  private String color = "white";
-  private boolean filled;
+    private String color = "white";
+    private boolean filled;
 
-  /**Default construct*/
-  protected GeometricObject() {
-  }
+    /**
+     * Default construct
+     */
+    protected GeometricObject() {
+    }
 
-  /**Construct a geometric object*/
-  protected GeometricObject(String color, boolean filled) {
-    this.color = color;
-    this.filled = filled;
-  }
+    /**
+     * Construct a geometric object
+     */
+    protected GeometricObject(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
 
-  /**Getter method for color*/
-  public String getColor() {
-    return color;
-  }
+    /**
+     * Getter method for color
+     */
+    public String getColor() {
+        return color;
+    }
 
-  /**Setter method for color*/
-  public void setColor(String color) {
-    this.color = color;
-  }
+    /**
+     * Setter method for color
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
 
-  /**Getter method for filled. Since filled is boolean,
-     so, the get method name is isFilled*/
-  public boolean isFilled() {
-    return filled;
-  }
+    /**
+     * Getter method for filled. Since filled is boolean,
+     * so, the get method name is isFilled
+     */
+    public boolean isFilled() {
+        return filled;
+    }
 
-  /**Setter method for filled*/
-  public void setFilled(boolean filled) {
-    this.filled = filled;
-  }
+    /**
+     * Setter method for filled
+     */
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
 
-  /**Abstract method findArea*/
-  public abstract double getArea();
+    /**
+     * Abstract method findArea
+     */
+    public abstract double getArea();
 
-  /**Abstract method getPerimeter*/
-  public abstract double getPerimeter();
+    /**
+     * Abstract method getPerimeter
+     */
+    public abstract double getPerimeter();
 }
